f792245afc5d9ae07a7b7b3bb15a6fa415151c15
Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>
2024-09-21 11:13:56 -0600

    Remove checks for invalid variables
    * These don't work, but creating the target works
    
    Signed-off-by: Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>

--- cmake/AddGeographiclibTarget.cmake
+++ cmake/AddGeographiclibTarget.cmake
@@ -3,14 +3,6 @@
 # libgeographic-dev 1.52 does not expose a target, so create it here.
 if(NOT TARGET GeographicLib::GeographicLib)
   add_library(GeographicLib::GeographicLib UNKNOWN IMPORTED)
-
-  # if(NOT "${GeographicLib_INCLUDE_DIR}")
-  #   message(FATAL_ERROR "The geographiclib variable GeographicLib_INCLUDE_DIR does not exist!")
-  # endif()
-
-  # if(NOT "${GeographicLib_LIBRARIES}")
-  #   message(FATAL_ERROR "The geographiclib variable GeographicLib_LIBRARIES does not exist!")
-  # endif()
   set_target_properties(GeographicLib::GeographicLib PROPERTIES
     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
     IMPORTED_LOCATION "${GeographicLib_LIBRARIES}"
